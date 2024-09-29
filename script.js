4de4d8d9ade09af9d5f88d03b6d1cb20f77d01b0
Raphael San <=>
2024-09-29 01:19:21 -0300

    subindo html para testar o azure

--- script.js
+++
@@ -1,9 +0,0 @@
-function mostrarResposta(id) {
-    const respostaDiv = document.querySelector(`.pergunta[data-id='${id}'] .resposta`);
-    respostaDiv.style.display = respostaDiv.style.display === 'none' ? 'block' : 'none';
-}
-
-function classificar(id, nota) {
-    const resultadoSpan = document.querySelector(`.pergunta[data-id='${id}'] .resultado`);
-    resultadoSpan.textContent = `Você classificou esta pergunta com ${nota} estrelas!`;
-}
